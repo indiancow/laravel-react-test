b6b2c8fb0452b2717f3fba84d35f9eb4ebd0dd03
indiancow <swadao326@gmail.com>
2023-10-03 17:27:18 +0900

    pivotっぽい色合いにする。

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,40 +1,18 @@
-// Navbar.jsx
 import React from 'react';
 import { InertiaLink } from '@inertiajs/inertia-react';
 
 function Navbar() {
     return (
         <nav>
-            <ul>
-                <li><InertiaLink href="/dashboard">Home</InertiaLink></li>
-                <li><InertiaLink href="/issues/create">クエスト作成</InertiaLink></li>
-                <li><InertiaLink href="/issues">クエスト一覧</InertiaLink></li>
-                <li><InertiaLink href="/feedbacks">フィードバック一覧</InertiaLink></li>
-                <li><InertiaLink href="/mypage">MyPage</InertiaLink></li>
+            <ul className="flex space-x-4">
+                <li className="text-white"><InertiaLink href="/dashboard" className="hover:underline">Home</InertiaLink></li>
+                <li className="text-white"><InertiaLink href="/issues/create" className="hover:underline">クエスト作成</InertiaLink></li>
+                <li className="text-white"><InertiaLink href="/issues" className="hover:underline">クエスト一覧</InertiaLink></li>
+                <li className="text-white"><InertiaLink href="/feedbacks" className="hover:underline">フィードバック一覧</InertiaLink></li>
+                <li className="text-white"><InertiaLink href="/mypage" className="hover:underline">MyPage</InertiaLink></li>
             </ul>
         </nav>
     );
 }
 
 export default Navbar;
-
-
-// // Navbar.jsx
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// function Navbar() {
-//     return (
-//         <nav>
-//             <ul>
-//                 <li><a href="/dashboard">Home</a></li>
-//                 <li><a href="/issues/create">クエスト作成</a></li>
-//                 <li><a href="/issues">クエスト一覧</a></li>
-//                 <li><a href="/feedbacks">フィードバック一覧</a></li>
-//                 {/* 他のナビゲーションリンクをこちらに追加 */}
-//             </ul>
-//         </nav>
-//     );
-// }
-
-// export default Navbar;
