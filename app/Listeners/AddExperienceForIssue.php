eb77055e27544f019115ba6b6ab478841a8effd7
indiancow <swadao326@gmail.com>
2023-10-12 23:17:20 +0900

    課題を投稿したら、ユーザーの探求者レベルが上がるリスナーに変更

--- app/Listeners/AddExperienceForIssue.php
+++ app/Listeners/AddExperienceForIssue.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use App\Models\User_skill;
 use App\Models\Issue;
+use App\Models\Skill;
 
 class AddExperienceForIssue
 {
@@ -25,27 +26,44 @@ class AddExperienceForIssue
      */
     public function handle(IssueCreated $event): void
     {
-        // イベントからユーザーIDと課題IDを取得
-        // dd($event);
         $userId = $event->userId;
         $issueId = $event->issueId;
-        // dd($issueId);
-        // 課題から関連するskill_idを取得
-        // ※ ここでは、Issueモデルが存在し、issueIdからskill_idが取得できる前提です。
-        $skillId = Issue::find($issueId)->tag_id;
-        
-        // 対象のユーザースキルを検索、もしくは新規作成
+
+        // 探求者スキルのIDを取得（ここではスキル名が'探求者'であると仮定しています）
+        $explorerSkillId = Skill::where('name', '探求者')->first()->id;
+
         $userSkill = User_Skill::firstOrCreate([
             'user_id' => $userId,
-            'skill_id' => $skillId,
+            'skill_id' => $explorerSkillId,
         ]);
 
-        // 経験値の付与処理
-        // ※ ここでは、経験値の量を$experiencePointsとしていますが、
-        // 実際の量や計算ロジックによって適切に変更してください。
-        $experiencePoints = 10; // 例: 10ポイントを付与
+        $experiencePoints = 10;
         $userSkill->increment('experience', $experiencePoints);
 
         event(new UserLevelUp($userSkill));
+
+
+        // イベントからユーザーIDと課題IDを取得
+        // dd($event);
+        // $userId = $event->userId;
+        // $issueId = $event->issueId;
+        // // dd($issueId);
+        // // 課題から関連するskill_idを取得
+        // // ※ ここでは、Issueモデルが存在し、issueIdからskill_idが取得できる前提です。
+        // $skillId = Issue::find($issueId)->tag_id;
+        
+        // // 対象のユーザースキルを検索、もしくは新規作成
+        // $userSkill = User_Skill::firstOrCreate([
+        //     'user_id' => $userId,
+        //     'skill_id' => $skillId,
+        // ]);
+
+        // // 経験値の付与処理
+        // // ※ ここでは、経験値の量を$experiencePointsとしていますが、
+        // // 実際の量や計算ロジックによって適切に変更してください。
+        // $experiencePoints = 10; // 例: 10ポイントを付与
+        // $userSkill->increment('experience', $experiencePoints);
+
+        // event(new UserLevelUp($userSkill));
     }
 }
